9a6b8ad06242afcccf1f99cd6d7cdf7863ecb5ea
yangjie <jie.yang@witontek.com>
2020-09-01 14:30:55 +0800

    update--

--- ngx-img-krump-app/projects/ngx-img-krump/src/lib/ngx-img-krump.component.ts
+++ ngx-img-krump-app/projects/ngx-img-krump/src/lib/ngx-img-krump.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-img-krump',
+  selector: 'ngx-img-krump',
   template: `
     <p>
       ngx-img-krump works!
--- ngx-img-krump-app/projects/ngx-img-krump/src/lib/ngx-img-krump.module.ts
+++ ngx-img-krump-app/projects/ngx-img-krump/src/lib/ngx-img-krump.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NgxImgKrumpComponent } from './ngx-img-krump.component';
 
-
-
 @NgModule({
   declarations: [NgxImgKrumpComponent],
   imports: [
